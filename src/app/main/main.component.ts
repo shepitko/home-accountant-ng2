8e108bfe8bba78b27e77c829b081f04a362c75c3
Юрий А. Шепитко <larch@foith.flor>
2016-06-09 17:21:28 +0300

    added service category

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,26 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { NbuService, Nbu } from '../nbu.service';
+import { CategoryService, Category } from '../category.service';
 
 @Component({
   moduleId: module.id,
   selector: 'app-main',
   templateUrl: 'main.component.html',
   styleUrls: ['main.component.css'],
-  providers: [NbuService]
+  providers: [NbuService, CategoryService]
 })
 export class MainComponent implements OnInit {
 
-  courses: any;
+  courses: Array<Nbu>;
+  categories: Array<Category>;
   selectedNbu: Nbu;
 
-  constructor(private nbuService: NbuService) {}
+  constructor(
+    private nbuService: NbuService, 
+    private categoryService: CategoryService
+    ) {}
 
   ngOnInit() {
-     this.nbuService.loadNbu()
-      .then(courses => {
-        this.courses = courses;
-      });
-      
-      /*console.log(this.nbuService.loadNbu());*/
+    this.nbuService.loadNbu()
+    .then(courses => {
+      this.courses = courses;
+    });
+
+    this.categoryService.loadAllCategories()
+    .then(categories => {
+      this.categories = categories;
+      console.log(categories);
+    });
   }
 }
