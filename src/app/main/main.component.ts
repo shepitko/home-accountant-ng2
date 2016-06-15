cd1652259d6f071b0ad24d83dbcaa711ea2d0513
Юрий А. Шепитко <larch@foith.flor>
2016-06-15 16:43:36 +0300

    added few html components

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AlertComponent, DATEPICKER_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
+import {CORE_DIRECTIVES} from '@angular/common';
+import { AlertComponent, DATEPICKER_DIRECTIVES, DROPDOWN_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
 import { NbuService, Nbu } from '../nbu.service';
 import { CategoryService, Category } from '../category.service';
 import { WalletService, Wallet } from '../wallet.service';
@@ -10,7 +11,7 @@ import { WalletService, Wallet } from '../wallet.service';
   templateUrl: 'main.component.html',
   styleUrls: ['main.component.css'],
   providers: [NbuService, CategoryService, WalletService],
-  directives: [AlertComponent, DATEPICKER_DIRECTIVES]
+  directives: [AlertComponent, DATEPICKER_DIRECTIVES, DROPDOWN_DIRECTIVES, CORE_DIRECTIVES]
 })
 export class MainComponent implements OnInit {
   date: Date = new Date();
@@ -21,6 +22,18 @@ export class MainComponent implements OnInit {
   wallets: Array<Wallet>;
   selectedNbu: Nbu;
   sum = {income:0, spend:0, total:0}
+  public disabled:boolean = false;
+  public status:{isopen:boolean} = {isopen: false};
+
+  public toggled(open:boolean):void {
+    console.log('Dropdown is now: ', open);
+  }
+
+  public toggleDropdown($event:MouseEvent):void {
+    $event.preventDefault();
+    $event.stopPropagation();
+    this.status.isopen = !this.status.isopen;
+  }
 
   constructor(
     private nbuService: NbuService, 
@@ -49,7 +62,7 @@ export class MainComponent implements OnInit {
     
     this.walletService.loadWallets()
     .then(wallets => {
-      this.wallets = wallets.reverse();
+      this.wallets = wallets;
     });
     
     this.walletService.loadWallets().then(wallets => {
